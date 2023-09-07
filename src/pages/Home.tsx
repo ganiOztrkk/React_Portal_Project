29690e344c0b72d9313859e5c5e5c2df3af641d4
Gani Ozturk <127308866+ganiOztrkk@users.noreply.github.com>
2023-09-07 17:32:43 +0300

    most common

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { getAllProducts } from '../Api'
-import { IProducts } from '../models/IProduct'
+import { IProducts } from '../models/IProducts'
 import ProductItem from '../components/ProductItem'
+import SimpleImageSlider from "react-simple-image-slider";
+
+// import images
+import img1 from '../assets/1.jpeg';
+import img2 from '../assets/2.jpeg';
+import NavBar from '../components/NavBar';
+import Header from '../components/Header';
 
 function Home() {
 
-    const [proObj, setProObj] = useState<IProducts>()
+  const images = [
+    { url: img1 },
+    { url: img2 },
+  ];
 
-    useEffect( () => {
-        getAllProducts().then( res => {
-            //işlem başarılı, datalar geldi
-            const dt = res.data
-            setProObj(dt)
-        } ).catch(err => {
-            //işlemde hata varsa;
-            alert("Servis Hatası Oluştu.")
-        })
-    }, [] )
+  const [proObj, setProObj] = useState<IProducts>()
+
+  useEffect( () => {
+    getAllProducts().then( res => {
+      // işlem başarılı, datalar geldi.
+      const dt = res.data
+      setProObj(dt)
+    }).catch(err => {
+      // işlemde hata varsa çalışacak kodlar.
+      alert('Servis Hatası Oluştu!')
+    })
+  }, [])
 
   return (
     <>
-        <div className="row" >
-            {proObj && proObj.products.map( (item, index) =>
-                <ProductItem item={item} key={index}/>
-            )}
-        </div>
+      <Header />
+      <div className='mb-3'>
+        <NavBar/>
+      </div>
+      <div className='mb-3' style={{ position: 'relative',}} >
+        <SimpleImageSlider
+          width={'100%'}
+          height={300}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+        />
+      </div>
+      <div className='row'>
+       { proObj && proObj.products.map( (item, index) => 
+          <ProductItem item={item} key={index} />
+       )}
+      </div>
     </>
   )
 }
